86078a85920f8b55a5ed8aab2f93594ec846dc1a
Marily46 <gonzalezmarily20@gmail.com>
2025-01-26 17:37:16 -0500

    Settings

---
+++ TiendaServicio.Api.Autor/Persistencia/ContextoAutor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaServicio.Api.Autor.Modelo;
+
+namespace TiendaServicio.Api.Autor.Persistencia
+{
+    public class ContextoAutor : DbContext
+    {
+        public ContextoAutor(DbContextOptions<ContextoAutor> options) : base(options) { }
+
+        public DbSet<AutorLibro> AutorLibro { get; set; }
+        public DbSet<GradoAcademico> GradoAcademico { get; set; }
+
+    }
+}
---
+++ TiendaServicio.Api.Autor/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using TiendaServicio.Api.Autor.Persistencia;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// entity framework with postgreSQL
+builder.Services.AddDbContext<ContextoAutor>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
